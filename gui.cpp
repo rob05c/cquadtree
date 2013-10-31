cdefb2b543c8b1a55e268829c0ec90659d096d82
Robert Butts <robert.o.butts@gmail.com>
2013-10-31 14:31:24 -0600

    improved gui

--- gui.cpp
+++ gui.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
-#include "quadtree.h"
-#include "free_quadtree.h"
 #include <atomic>
 #include <memory>
 #include <thread>
 #include <chrono>
-
+#include <string>
+#include <unordered_map>
 #include <ncurses.h>
-
+#include "quadtree.h"
+#include "free_quadtree.h"
 namespace
 {
 using std::vector;
@@ -21,6 +21,9 @@ using std::srand;
 using std::time;
 using std::max;
 using std::strtoul;
+using std::string;
+using std::to_string;
+using std::unordered_map;
 using std::chrono::time_point;
 using std::chrono::system_clock;
 using std::chrono::duration;
@@ -31,12 +34,78 @@ using quadtree::LockfreeQuadtree;
 
 const unsigned int NODE_CAPACITY = 100;
 const unsigned int DEFAULT_THREADS = max(thread::hardware_concurrency(), 1u);
-const unsigned int DEFAULT_POINTS = 10000000;
+const unsigned int DEFAULT_POINTS = 50000;
 
 inline double frand()
 {
   return (double)rand() / (double)RAND_MAX;
 }
+
+const char* NUM[] = {
+  u8" ",
+  u8"\u00b7",
+  u8"\u205a",
+  u8"\u2056",
+  u8"\u2058",
+  u8"\u2059",
+  u8"\u28f6",
+  u8"\u28f7",
+  u8"\u28ff",
+  u8"\u2468",
+  u8"\u2469",
+  u8"\u246a",
+  u8"\u246b",
+  u8"\u246c",
+  u8"\u246d",
+  u8"\u246e",
+  u8"\u246f",
+  u8"\u2470",
+  u8"\u2471",
+  u8"\u2472",
+  u8"\u2473",
+  u8"\u3251",
+  u8"\u3252",
+  u8"\u3253",
+  u8"\u3254",
+  u8"\u3255",
+  u8"\u3256",
+  u8"\u3257",
+  u8"\u3258",
+  u8"\u3259",
+  u8"\u325a",
+  u8"\u325b",
+  u8"\u325c",
+  u8"\u325d",
+  u8"\u325e",
+  u8"\u325f",
+  u8"\u32b1",
+  u8"\u32b2",
+  u8"\u32b3",
+  u8"\u32b4",
+  u8"\u32b5",
+  u8"\u32b6",
+  u8"\u32b7",
+  u8"\u32b8",
+  u8"\u32b9",
+  u8"\u32ba",
+  u8"\u32bb",
+  u8"\u32bc",
+  u8"\u32bd",
+  u8"\u32be",
+  u8"\u32bf"
+};
+const size_t NUM_LEN = sizeof(NUM) / sizeof(NUM[0]);
+
+const char* MANY = u8"\u2603";
+
+inline const char* numstr(const size_t& n)
+{
+//  return u8"\u00b7";
+  return n > NUM_LEN ? MANY : NUM[n];
+}
+
+int window_height = 0;
+int window_width = 0;
 }
 
 
@@ -46,75 +115,68 @@ inline double frand()
 /// @return the number of points inserted. This will equal floor(points/threads)*threads, not points
 int testInsert(Quadtree* q, int points, int numThreads)
 {
+  const BoundingBox b = q->Boundary();
+  const auto width = b.Center.X + b.HalfDimension.X;
+  const auto height = b.Center.Y + b.HalfDimension.Y;
   const auto tpoints = points / numThreads;
-  const auto insertPoint = [tpoints, q] () {
+  const auto insertPoint = [tpoints, q, width, height] () {
     for(int i = 0, end = tpoints; i != end; ++i)
     {
-      const auto p = Point(frand()*100.0 + 50.0, frand() * 100.0 + 50.0);
-      const bool ok = q->Insert(p);
-      if(!ok)
-	cout << "testInsert insert failed" << endl;
-
-      // debug
-/*
-      if(i % 100000 == 0)
-      {
-	const std::string msg = std::string() + "inserted " + std::to_string(i) + "\n";
-	cout << msg;
-      }
-*/
+      q->Insert(Point(frand()*(width-1) + 1, frand()*(height-1) + 1));
     }
   };
 
-  cout << "inserting " << tpoints << " per thread\n";  
-
   vector<shared_ptr<thread>> threads;
   for(int i = 0, end = numThreads; i != end; ++i)
-  {
-//    cout << "starting thread\n";
     threads.push_back(shared_ptr<thread>(new thread(insertPoint)));
-  }
-
   for(auto i : threads)
-  {
     i->join();
-//    cout << "joined thread\n";
-  }
 
   return tpoints * numThreads;
 }
 
-void printTree(Quadtree* q)
+/// prints basic GUI that should always be visible
+void printGui(const string& msg)
 {
-  vector<Point> ps = q->Query(q->Boundary());
-  cout << "queried " << ps.size() << endl;
-  if(ps.size() < 1000)
-  {
-    cout << "found ";
-    for(auto i = ps.begin(), end = ps.end(); i != end; ++i)
-      cout << "(" << i->X << " " << i->Y << "), ";
-    cout << endl;
-  }
+  box(stdscr, 0, 0);
+  mvprintw(window_height, window_width / 2 - msg.size() / 2, msg.c_str());
+  mvprintw(window_height, 1,"%s", "Quadtree demo ");
+  const string msgExit = " Press any key to exit";
+  mvprintw(window_height, window_width - msgExit.size() - 1, msgExit.c_str());
+}
+
+/// Does NOT refresh the window.
+/// @return tree info message
+string drawTree(Quadtree* q)
+{
+
+  vector<Point> points = q->Query(q->Boundary());
+
+  unordered_map<long, unordered_map<long, size_t>> pointMap;
+  for(auto i = points.begin(), end = points.end(); i != end; ++i)
+    ++pointMap[(int)i->X][(int)i->Y];
+
+  for(auto i = pointMap.begin(), end = pointMap.end(); i != end; ++i)
+    for(auto j = i->second.begin(), jend = i->second.end(); j != jend; ++j)
+      mvprintw(j->first, i->first, numstr(j->second));
+
+  return string() + "points: " + to_string(points.size());
 }
 
 int main(int argc, char** argv)
 {
-  // @todo cout whether unsigned long is atomic!!
-
-//  cout << sizeof(int*);
-
+  setlocale(LC_ALL, "");
   initscr();
-  printw("Quadtree demo\n");
-  printw("Press any key to exit");
+  noecho();
+
+  getmaxyx(stdscr, window_height, window_width);
+  --window_height;
+
+  printGui("loading...");
   refresh();
-  getch();
-  endwin();
 
-
-/*
   auto points = DEFAULT_POINTS;
   auto threads = DEFAULT_THREADS;
-
   if(argc > 1)
   {
     const auto p = static_cast<unsigned int>(strtoul(argv[1], 0, 10));
@@ -129,49 +191,17 @@ int main(int argc, char** argv)
   }
 
   srand(time(nullptr));
-  cout << std::fixed;
 
-  cout << "threads: " << threads << endl;
-  cout << "points: " << points << endl;
-  cout << "capacity: " << NODE_CAPACITY << endl;
-
-  //#if !__has_feature(cxx_atomic)
-  //  cout << "no atomic :(" << endl;
-  //#endif
-
-  const BoundingBox b = {{100, 100}, {50, 50}};
+  Point center = {(double)(window_width / 2), (double)(window_height / 2)};
+  const BoundingBox b = {center, center}; // half-dimension is same as center, for our screen-size box
   LockfreeQuadtree q(b, NODE_CAPACITY);
 
-  **
-  auto qpoints = q.points.load();
-  if(qpoints->First == nullptr)
-    cout << "first null\n";
-  else if(qpoints->First->Next == nullptr)
-    cout << "first->next null\n";
-
-  const auto p = Point(frand()*100.0 + 50.0, frand() * 100.0 + 50.0);
-  const bool ok = q.Insert(p);
-
-  qpoints = q.points.load();
-  if(qpoints->First == nullptr)
-    cout << "2 first null\n";
-  else if(qpoints->First->Next == nullptr)
-    cout << "2 first->next null\n";
-    **
-
-  const time_point<system_clock> start = system_clock::now();
-
-
-
-  const int inserted = testInsert(&q, points, threads);
-
-  const time_point<system_clock> end = system_clock::now();
-  const duration<double> elapsed = end - start;
-  const int seconds_elapsed = elapsed.count();
-
-  cout << "inserted " << inserted << " in " << seconds_elapsed << " seconds with " << threads << " threads." << endl;
-
-  printTree(&q);
-*/
+  testInsert(&q, points, threads);
+  const string treeMsg = drawTree(&q);
+  printGui(treeMsg);
+  curs_set(0); // hide cursor
+  refresh();
+  getch();
+  endwin();
   return 0;
 }
